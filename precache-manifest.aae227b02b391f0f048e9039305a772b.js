b5b3fb6c7ed8d076952d28ec752e55be8e5a11ea
Rameezahmad <rameez007.ra@gmail.com>
2020-04-02 13:29:25 +0500

    Updates

---
+++ precache-manifest.aae227b02b391f0f048e9039305a772b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f45656c1f0e3ed9efa297dcd553189",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "7e4c2d95db42f5d3a908",
+    "url": "/robo-friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e2767fb1ad000a2647c3",
+    "url": "/robo-friends/static/css/main.c05d0ea7.chunk.css"
+  },
+  {
+    "revision": "7e4c2d95db42f5d3a908",
+    "url": "/robo-friends/static/js/2.7bc29fa8.chunk.js"
+  },
+  {
+    "revision": "e2767fb1ad000a2647c3",
+    "url": "/robo-friends/static/js/main.0daf84f4.chunk.js"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
